f05764f83f9da52850693ce781de83710fba4c44
Serg <sergey.gabrielyan1998@gmail.com>
2023-05-24 20:15:19 +0400

    Add menu, make it responsive and install material-tailwind package

---
+++ src/constants/index.js
@@ -0,0 +1,22 @@
+export const navLinks = [
+  {
+    id: "collections",
+    title: "Collections",
+  },
+  {
+    id: "men",
+    title: "Men",
+  },
+  {
+    id: "women",
+    title: "Women",
+  },
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
