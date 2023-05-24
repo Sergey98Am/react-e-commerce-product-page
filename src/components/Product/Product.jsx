8258b00bdcd6188ec463d6a067ce3e4bada15955
Serg <sergey.gabrielyan1998@gmail.com>
2023-05-24 21:24:31 +0400

    Update product layouts

---
+++ src/components/Product/Product.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Product = () => {
+  return (
+    <div className="product px-6">
+      <div className="container mx-auto mt-[90px]">
+        <div className="grid grid-cols-2 gap-[125px] mx-12">
+          <div className="bg-tertiary">6</div>
+          <div className="bg-tertiary">6</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Product;
