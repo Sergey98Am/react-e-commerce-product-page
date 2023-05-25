05f1159519f885b6d8fdc31b4882262279347bcc
Serg <sergey.gabrielyan1998@gmail.com>
2023-05-25 23:38:11 +0400

    Add thumbanils slider with splide package, make it responsive and add scss

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import "./main.scss";
 import App from "./App.jsx";
 import { ThemeProvider } from "@material-tailwind/react";
+import '@splidejs/react-splide/css';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
